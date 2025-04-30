df4148090bb8f6306d143284c35f93be2f30c127
Mmaduabuchi Elvis Igwilo <148869665+Elhefeelvis1@users.noreply.github.com>
2025-04-30 18:31:16 +0100

    updated git ignore and other files

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import express from 'express';
-import ejs from 'ejs'
+import ejs from 'ejs';
+import pg from 'pg';
+import db from "./dbConn"
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+db.connect();
+
+app.get("/", async (req, res) => {
+    res.render('index.ejs',{
+
+    })
+})
